73fb9854e3c179c2755d6092b8b05edc4bf7060d
wsnd112 <88569165+wsnd112@users.noreply.github.com>
2022-04-07 22:03:03 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -23,66 +23,199 @@
 //	printf("%d", sizeof(struct Node));
 //	return 0;
 //}
-void* my_memcpy(void* e1, const void* e2, size_t sz)
-{
-    assert(e1 && e2);
-    void* ret = e1;
-    while (sz--)
-    {
-        *(char*)e1 = *(char*)e2;
-        ++(char*)e1;
-        ++(char*)e2;
-    }
-    return ret;
-}
-void* my_memmove(void* e1, void* e2, size_t sz)
-{
-    assert(e1 && e2);
-    void* ret = e1;
-    if (e1 > e2)
-    {
-        while (sz--)
-        {
-            *((char*)e1 + sz) = *((char*)e2 + sz);
-        }
-    }
-    else
-    {
-        while (sz--)
-        {
-            *(char*)e1 = *(char*)e2;
-            ++(char*)e1;
-            ++(char*)e2;
-        }
-    }
-    return ret;
-}
+//void* my_memcpy(void* e1, const void* e2, size_t sz)
+//{
+//    assert(e1 && e2);
+//    void* ret = e1;
+//    while (sz--)
+//    {
+//        *(char*)e1 = *(char*)e2;
+//        ++(char*)e1;
+//        ++(char*)e2;
+//    }
+//    return ret;
+//}
+//void* my_memmove(void* e1, void* e2, size_t sz)
+//{
+//    assert(e1 && e2);
+//    void* ret = e1;
+//    if (e1 > e2)
+//    {
+//        while (sz--)
+//        {
+//            *((char*)e1 + sz) = *((char*)e2 + sz);
+//        }
+//    }
+//    else
+//    {
+//        while (sz--)
+//        {
+//            *(char*)e1 = *(char*)e2;
+//            ++(char*)e1;
+//            ++(char*)e2;
+//        }
+//    }
+//    return ret;
+//}
+//
+//typedef struct stu
+//{
+//    char name[20];
+//    int age;
+//}s;
+//int main()
+//{
+//    /*s arr1[] = { {"zhangsan",20},{"lisi",18} };
+//    s arr2[5] = { 0 };*/
+//    /* int arr1[] = { 1,2,3,4,5 };
+//     int arr2[6] = { 0,0,0,0,0,9 };
+//     my_memcpy(arr2, arr1, sizeof(arr1));*/
+//     /* int i;
+//      for (i = 0; i < sizeof(arr2) / sizeof(arr2[0]); i++)
+//      {
+//          printf("%d ", arr2[i]);
+//      }*/
+//    int arr[] = { 1,2,3,4,5,6,7,8,9 };
+//    //my_memcpy(&arr[2], arr, 20);
+//    my_memmove(&arr[2], arr, 20);
+//    //my_memmove(arr, &arr[2], 20);
+//    int i = 0;
+//    for (i = 0; i < 9; i++)
+//    {
+//        printf("%d ", arr[i]);
+//    }
+//    return 0;
+//}
+//#pragma pack(4)//设置默认对齐数为4;
+//struct S
+//{
+//	char a;
+//	double b;
+//};
+//#pragma pack()//取消设置的默认对齐数;
+//int main()
+//{
+//	struct S s = { 0 };
+//	printf("%d", sizeof(s));
+//	return 0;
+//}
+//#include<stddef.h>//offsetof()--计算偏移量;
+//struct S
+//{
+//	char a;
+//	int b;
+//	double d;
+//};
+//int main()
+//{
+//	printf("%d\n", offsetof(struct S, a));
+//	printf("%d\n", offsetof(struct S, b));
+//	printf("%d\n", offsetof(struct S, d));
+//	return 0;
+//}
 
-typedef struct stu
+//struct S
+//{
+//	int a;
+//	char c;
+//	double d;
+//};
+//
+//void Init(struct S* ps)
+//{
+//	ps->a = 10;
+//	ps->c = 'W';
+//	ps->d = 3.14;
+//}
+//void Print1(struct S tmp)
+//{
+//	printf("%d %c %lf\n", tmp.a, tmp.c, tmp.d);
+//}
+//void Print2(const struct S* ps)
+//{
+//	printf("%d %c %lf\n", ps->a, ps->c, ps->d);
+//}
+//int main()
+//{
+//	struct S s = { 0 };
+//	Init(&s);
+//	Print1(s);
+//	Print2(&s);
+//	return 0;
+//}
+// 
+//位段
+typedef struct S
 {
-    char name[20];
-    int age;
+	char a : 3;
+	char b : 4;
+	char c : 5;
+	char d : 4;
 }s;
+
 int main()
 {
-    /*s arr1[] = { {"zhangsan",20},{"lisi",18} };
-    s arr2[5] = { 0 };*/
-    /* int arr1[] = { 1,2,3,4,5 };
-     int arr2[6] = { 0,0,0,0,0,9 };
-     my_memcpy(arr2, arr1, sizeof(arr1));*/
-     /* int i;
-      for (i = 0; i < sizeof(arr2) / sizeof(arr2[0]); i++)
-      {
-          printf("%d ", arr2[i]);
-      }*/
-    int arr[] = { 1,2,3,4,5,6,7,8,9 };
-    //my_memcpy(&arr[2], arr, 20);
-    my_memmove(&arr[2], arr, 20);
-    //my_memmove(arr, &arr[2], 20);
-    int i = 0;
-    for (i = 0; i < 9; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    return 0;
+	s a = { 0 };
+	a.a = 10;
+	a.b = 20;
+	a.c = 3;
+	a.d = 4;
+	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
